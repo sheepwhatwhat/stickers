d4df8b5cd6543bc9088bb97a414d10119eabb2d8
darkobits <joshua+github@frontlawn.net>
2021-01-21 21:08:22 -0800

    chore: Add jest-github-reporter

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,6 @@
 module.exports = require('@darkobits/ts').jest({
-  setupFilesAfterEnv: ['jest-expect-message'],
-  moduleFileExtensions: [
-    'js',
-    'json',
-    'node',
-    'ts',
-    'yml'
-  ],
-  clearMocks: true
-  // coverageThreshold: {
-  //   global: {
-  //     statements: 100,
-  //     branches: 95,
-  //     functions: 100,
-  //     lines: 100
-  //   }
-  // }
+  // setupFilesAfterEnv: ['jest-expect-message'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node', 'yml'],
+  // clearMocks: true,
+  reporters: ['default', 'jest-github-reporter']
 });
--- package-scripts.js
+++ package-scripts.js
@@ -1,5 +1,9 @@
 module.exports = require('@darkobits/ts').nps({
   scripts: {
-    compile: 'npx compile-stickers --input-file src/stickers.yml --output-file partials/partials.json'
+    compile: 'npx compile-stickers --input-file src/stickers.yml --output-file partials/partials.json',
+    // Overwrite the default 'test' script, adding the --testLocationInResults
+    // flag required by the jest-github-reporter.
+    // See: https://github.com/hipstersmoothie/jest-github-reporter#usage
+    test: 'ts.jest --testLocationInResults'
   }
 });
